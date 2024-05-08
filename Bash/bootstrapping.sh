23d91cf5b7881cdc85ca73d778e4ccc047d7c3ae
MatveyGuralskiy <mathewguralskiy@gmail.com>
2024-05-08 07:19:40 +0300

    Add Bash directory for Bootstrapping on Instances

---
+++ Bash/bootstrapping.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Script for EC2 Instances to run Docker Containers with Application
+#-------------------------------------
+#Created by Matvey Guralskiy
+
+#Install Docker with script
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+# To run docker
+sudo systemctl start docker
+sudo systemctl enable docker
+# To Install my Application from DockerHub
+sudo docker pull matveyguralskiy/auto-deploy:V1.0
+# To run the Docker Container on port HTTP
+sudo docker run -d -p 80:80 matveyguralskiy/auto-deploy:V1.0
