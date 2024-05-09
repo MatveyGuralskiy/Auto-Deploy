1f95eb73b833c087d57ab9dbd12add863aa993f2
Matvey Guralskiy <mathewguralskiy@gmail.com>
2024-05-09 09:35:29 +0300

    Update Jenkinsfile.groovy

--- Jenkins/Jenkinsfile.groovy
+++ Jenkins/Jenkinsfile.groovy
@@ -54,8 +54,8 @@ pipeline {
                     try {
                         withCredentials([usernamePassword(credentialsId: 'dockerhub', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
                             sh 'docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD'
-                            sh 'docker tag auto-deploy:V1.0 matveyguralskiy/auto-deploy:${env.DOCKER_VERSION}'
-                            sh 'docker push matveyguralskiy/auto-deploy:${env.DOCKER_VERSION}'
+                            sh 'docker tag auto-deploy:V1.0 matveyguralskiy/auto-deploy:V1.1'
+                            sh 'docker push matveyguralskiy/auto-deploy:V1.1'
                             echo "Docker Image uploaded"
                         }
                     } catch (Exception e) {
@@ -69,7 +69,7 @@ pipeline {
                 script {
                     try {
                         sh 'docker pull matveyguralskiy/auto-deploy:V${env.DOCKER_VERSION}'
-                        sh 'docker run -d --name test-container -p 7000:80 matveyguralskiy/auto-deploy:${env.DOCKER_VERSION}'
+                        sh 'docker run -d --name test-container -p 7000:80 matveyguralskiy/auto-deploy:V1.1'
                         def response = sh(script: 'curl -s -o /dev/null -w "%{http_code}" localhost', returnStdout: true).trim()
                         if (response == '200') {
                             echo "Application page is accessible"
